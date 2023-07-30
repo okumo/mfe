01baeb78f3d28c6c8251d9df3170a3274bf3ee50
Oscar Caballero <oscar.kballero@hotmail.com>
2023-07-30 18:31:15 -0500

    second commit

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -1,1 +1,23 @@
+const { merge } = require("webpack-merge");
+const ModuleFedarationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
 
+const prodConfig = {
+  mode: "production",
+  output: {
+    fileName: "[name].[contenthash].js",
+  },
+  plugins: [
+    new ModuleFedarationPlugin({
+      name: "marketing",
+      fileName: "remoteEntry.js",
+      exposes: {
+        "./MarketingApp": "./src/bootstrap",
+      },
+      shared: packageJson.dependencies,
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, prodConfig);
