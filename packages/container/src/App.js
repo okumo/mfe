88fdd1375d576ea01a3094086ccf94c74bd73faa
Oscar Caballero <oscar.kballero@hotmail.com>
2023-09-03 16:40:49 -0500

    added auth aubapp

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,23 +1,39 @@
-import React from "react";
-import MarketingApp from "./components/MarketingApp";
+import React, { useState } from "react";
 import Header from "./components/Header";
-import { BrowserRouter } from "react-router-dom";
+import Progress from "./components/Progress";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import {
   createGenerateClassName,
   StylesProvider,
 } from "@material-ui/core/styles";
+import { lazy } from "react";
+import { Suspense } from "react";
+
+const MarketingLazy = lazy(() => import("./components/MarketingApp"));
+const AuthLazy = lazy(() => import("./components/AuthApp"));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: "co",
 });
 export default () => {
+  const [isSignedIn, setIsSignedIn] = useState(false);
   return (
     <BrowserRouter>
       <StylesProvider generateClassName={generateClassName}>
         <div>
-          <Header />
-          <MarketingApp />
+          <Header
+            isSignedIn={isSignedIn}
+            onSignOut={() => setIsSignedIn(false)}
+          />
+          <Suspense fallback={<Progress />}>
+            <Switch>
+              <Route path="/auth">
+                <AuthLazy onSignIn={() => setIsSignedIn(true)} />
+              </Route>
+              <Route path="/" component={MarketingLazy} />
+            </Switch>
+          </Suspense>
         </div>
       </StylesProvider>
     </BrowserRouter>
